1c7506f8818ad4116bc9ed3b3e6c315380286950
Shivani Sharma <92907798+05912Shivani@users.noreply.github.com>
2025-08-02 22:31:08 +0530

    authSlice.js

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -1,22 +1,26 @@
 // src/redux/slices/authSlice.js
 
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';// Import createSlice to define a Redux slice using Redux Toolkit
 
-const savedUser = JSON.parse(localStorage.getItem('userInfo'));
+const savedUser = JSON.parse(localStorage.getItem('userInfo'));// Try to retrieve any saved user info from localStorage (helps persist login state)
 
+// Define the initial authentication state
 const initialState = {
   isAuthenticated: !!savedUser,
   user: savedUser || null,
 };
 
+// Create a slice named 'auth' to manage authentication-related state and actions
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
+       // Handle user login
     login: (state, action) => {
       state.isAuthenticated = true;
       state.user = action.payload;
     },
+     // Handle user logout
     logout: (state) => {
       state.isAuthenticated = false;
       state.user = null;
