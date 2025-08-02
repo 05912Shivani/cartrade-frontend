4fe410df318b5708fe8ae8c7603101e7478e714e
Shivani Sharma <92907798+05912Shivani@users.noreply.github.com>
2025-08-02 22:26:13 +0530

    store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,15 @@
 // src/redux/store.js
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';// Import the configureStore function from Redux Toolkit
+// Import individual reducers from their respective slices
 import carReducer from './slices/carSlice';
 import cartReducer from './cartSlice';
 import authReducer from './slices/authSlice'; 
 
 const store = configureStore({
   reducer: {
-    car: carReducer,
-    cart: cartReducer,
-    auth: authReducer, 
+    car: carReducer,// Accessible via state.car in components
+    cart: cartReducer,// Accessible via state.cart
+    auth: authReducer, // Accessible via state.auth
   },
 });
 
