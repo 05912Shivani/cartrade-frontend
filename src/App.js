6f8b8d83e450629e888d290e361fea6966651327
Shivani Sharma <92907798+05912Shivani@users.noreply.github.com>
2025-08-04 16:35:38 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Home from './pages/Home';
-// import CarDetail from './pages/CarDetail';
-// import Login from './pages/Login';
-// import Signup from './pages/Signup';
-// import Cart from './pages/Cart';
-// import Navbar from './components/Navbar';
-// import Footer from './components/Footer';
-// import SellCar from './pages/SellCar';
-// import ServicesPage from './pages/ServicesPage';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/car/:id" element={<CarDetail />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/signup" element={<Signup />} />
-//         <Route path="/cart" element={<Cart />} />
-//         <Route path="/sell-car" element={<SellCar />} />
-//         <Route path="/services" element={<ServicesPage />} />
-//       </Routes>
-//       <Footer />
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CarDetail from './pages/CarDetail';
